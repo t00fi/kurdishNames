10bf5806a02dd49578e98e7ac1b1729560187df0
Mustafa <mcho.khalid@gmail.com>
2022-07-06 16:07:43 +0300

    adding comment

--- kurdish_names/lib/services/kurdish_name_request_api.dart
+++ kurdish_names/lib/services/kurdish_name_request_api.dart
@@ -4,13 +4,16 @@ import 'package:kurdish_names/model/name_data_modal.dart';
 class RequestNames {
 // https://github.com/DevelopersTree/nawikurdi
 // https://nawikurdi.com/
-
 //API end point : https://nawikurdi.com/api
+
+//create method with type future to send request to the api .
   Future<KurdishNamesModal> getnames(String limit, String gender) async {
+    //create url to send request for it.
     Uri url = Uri(
       scheme: 'https',
       host: 'nawikurdi.com',
       path: 'api',
+      //query paramaeter are used for get request.
       queryParameters: {
         'limit': limit,
         'gender': gender,
@@ -18,6 +21,8 @@ class RequestNames {
       },
     );
 
+    //get response from the api if there is any data
+    //send get() request to the api
     http.Response response =
         await http.get(Uri.parse(url.toString())).catchError(
               (err) => print(err),
